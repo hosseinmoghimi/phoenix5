d7c69e5b740adc8ee4de110f96ab14429967f00a
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2022-03-04 21:58:31 +0330

    initial project phoenix

---
+++ phoenix/settings.py
@@ -0,0 +1,112 @@
+
+from . import server_settings
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY=server_settings.SECRET_KEY
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = server_settings.DEBUG
+
+QRCODE_ROOT=server_settings.QRCODE_ROOT
+# Application definition
+
+INSTALLED_APPS =server_settings.INSTALLED_APPS
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'phoenix.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'phoenix.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES=server_settings.DATABASES
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = server_settings.TIME_ZONE
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_URL = server_settings.STATIC_URL
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = server_settings.ALLOWED_HOSTS
+TIME_ZONE = server_settings.TIME_ZONE
+
+SITE_URL=server_settings.SITE_URL
+
+STATIC_ROOT=server_settings.STATIC_ROOT
+MEDIA_ROOT=server_settings.MEDIA_ROOT
+STATIC_URL = server_settings.STATIC_URL
+MEDIA_URL =  server_settings.MEDIA_URL
+ADMIN_URL =  server_settings.ADMIN_URL
+UPLOAD_ROOT =  server_settings.UPLOAD_ROOT
+
+STATICFILES_DIRS=server_settings.STATICFILES_DIRS
---
+++ phoenix/urls.py
@@ -0,0 +1,21 @@
+
+from django.contrib import admin
+from django.urls import path,include,re_path
+
+
+from .settings import MEDIA_URL, MEDIA_ROOT, STATIC_URL, STATIC_ROOT, DEBUG,QRCODE_ROOT
+from django.views.static import serve
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('authentication/', include('authentication.urls')),
+    # path('core/', include('core.urls')),
+    # path('dashboard/', include('dashboard.urls')),
+    # path('market/', include('market.urls')),
+    # path('entity/', include('entity.urls')),
+    # path('accounting/', include('accounting.urls')),
+    re_path(r'^qrcode/(?P<path>.*)$', serve, {'document_root': QRCODE_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
+]
