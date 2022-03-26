0652aafdd5904007caf47f4f3e955db4fc0c9677
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2022-03-27 03:20:15 +0430

    edit for folder

--- archive/serializers.py
+++ archive/serializers.py
@@ -5,4 +5,4 @@ from archive.models import Folder
 class FolderSerializer(serializers.ModelSerializer):
     class Meta:
         model=Folder
-        fields=['id','name','get_absolute_url','get_breadcrumb']
+        fields=['id','name','get_absolute_url','get_breadcrumb','get_edit_url']
