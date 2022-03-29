453f4deaffd85cb700e25d20acb63ddd6ff5ea89
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2022-03-30 02:01:19 +0430

    local urls added

--- phoenix/urls.py
+++ phoenix/urls.py
@@ -1,34 +1,5 @@
 
-from django.contrib import admin
-from django.urls import path,include,re_path
-
-from .settings import MEDIA_URL, MEDIA_ROOT, STATIC_URL, STATIC_ROOT, DEBUG,QRCODE_ROOT
-from django.views.static import serve
-
+from phoenix.server_urls import urlpatterns as server_url_patterns
 # from web3auth import urls as web3auth_urls
 
-urlpatterns = [
-    path('', include('accounting.urls')),
-    path('admin/', admin.site.urls),
-    path('accounting/', include('accounting.urls')),
-    path('authentication/', include('authentication.urls')),
-    path('core/', include('core.urls')),
-    path('dashboard/', include('dashboard.urls')),
-    path('market/', include('market.urls')),
-    path('pm/', include('projectmanager.urls')),
-    path('web/', include('web.urls')),
-    path('transport/', include('transport.urls')),
-    path('map/', include('map.urls')),
-    path('stock/', include('stock.urls')),
-    path('guarantee/', include('guarantee.urls')),
-    path('log/', include('log.urls')),
-    path('archive/', include('archive.urls')),
-    path('messenger/', include('messenger.urls')),
-    path('wallet/', include('wallet.urls')),
-    path('warehouse/', include('warehouse.urls')),
-    # re_path(r'^', include(web3auth_urls)),
-# web3auth_urls
-    re_path(r'^qrcode/(?P<path>.*)$', serve, {'document_root': QRCODE_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': STATIC_ROOT}),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
-]
+urlpatterns = server_url_patterns
