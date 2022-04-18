05cde793c39fb9f9e9891ff29231591409e63b7d
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2022-04-19 01:54:25 +0430

    ....

--- market/serializers.py
+++ market/serializers.py
@@ -3,19 +3,25 @@ from market.models import Category,Product
 from accounting.serializers import ProductSerializer as ProductSerializer_origin
 
  
-
-class ProductSerializer(ProductSerializer_origin):
-    pass
+ 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=Category
-        fields=['id','title','get_absolute_url']
+        fields=['id','title','thumbnail','get_absolute_url']
 class CategorySerializerForApi(serializers.ModelSerializer):
     class Meta:
         model=Category
         fields=['id','title','thumbnail','get_absolute_url','parent_id','get_products_list_url']
 
+
+
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields=['id','title','thumbnail','get_absolute_url']
 class ProductSerializerForApi(serializers.ModelSerializer):
     class Meta:
         model=Product
-        fields=['id','title','thumbnail','get_absolute_url',]
+        fields=['id','title','thumbnail','get_absolute_url']
