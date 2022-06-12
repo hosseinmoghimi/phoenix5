827bc08ecaca40b55d075dc03a0e5c36b2a94169
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2022-06-12 05:20:37 +0430

    bugs fixed

--- utility/templatetags/to_absolute_link.py
+++ utility/templatetags/to_absolute_link.py
@@ -8,4 +8,8 @@ def to_absolute_link(url):
         absolute_link=SITE_FULL_BASE_ADDRESS[0:len(SITE_FULL_BASE_ADDRESS)-1]+url
     else:
         absolute_link=SITE_FULL_BASE_ADDRESS+url
+    if absolute_link[len(absolute_link)-1]=="/":
+        pass
+    else:
+        absolute_link=absolute_link+"/"
     return absolute_link
