3276a275bdf4294acdcf6e2ff9b222c27d6a530e
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2022-08-07 04:21:30 +0430

    ,,,

--- projectmanager/forms.py
+++ projectmanager/forms.py
@@ -77,7 +77,7 @@ class AddSignatureForm(forms.Form):
     service_request_id=forms.IntegerField(required=False)
     material_request_id=forms.IntegerField(required=False)
     request_id=forms.IntegerField(required=True)
-    description=forms.CharField(max_length=50,required=False)
+    description=forms.CharField(max_length=500,required=False)
     status=forms.CharField(max_length=50,required=True)
 
 
