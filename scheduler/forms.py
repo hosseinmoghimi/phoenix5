15f026fe5a594f892580ebc1af970df9a9dc1194
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2022-08-14 04:19:26 +0430

    bugs fixed

--- scheduler/forms.py
+++ scheduler/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class AddAppointmentForm(forms.Form):
     title=forms.CharField(max_length=100, required=True)
-    status=forms.CharField(max_length=100, required=True)
-    color=forms.CharField(max_length=100, required=True)
+    status=forms.CharField(max_length=50, required=True)
+    color=forms.CharField(max_length=50, required=True)
     date_fixed=forms.CharField(max_length=30, required=True)
     location_id=forms.IntegerField(required=True)
