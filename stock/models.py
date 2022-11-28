1ef9ab03898f2307ae05ee4c30470a9ae343271e
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2022-11-28 23:24:46 +0330

    bugs fixed

--- stock/models.py
+++ stock/models.py
@@ -1,20 +1,22 @@
 import profile
 from django.db import models
+from core.models import LinkHelper
 from django.utils.translation import gettext as _
 from django.shortcuts import reverse
+from stock.apps import APP_NAME
 
-class ShareHolder(models.Model):
-    profile=models.ForeignKey("authentication.profile", verbose_name=_("profile"), on_delete=models.CASCADE)
-    stock=models.IntegerField(_("میزان مشارکت"))
+class ShareHolder(models.Model,LinkHelper):
+    # account=models.ForeignKey("accounting.account", verbose_name=_("account"), on_delete=models.CASCADE)
     account=models.ForeignKey("accounting.account", verbose_name=_("account"), on_delete=models.CASCADE)
-    
+    asset=models.ForeignKey("accounting.asset", verbose_name=_("asset"), on_delete=models.CASCADE)
+    stock=models.IntegerField(_("میزان مشارکت"))
+    class_name="shareholder"
+    app_name=APP_NAME
 
     class Meta:
         verbose_name = _("ShareHolder")
         verbose_name_plural = _("ShareHolders")
 
     def __str__(self):
-        return self.profile.name
-
-    def get_absolute_url(self):
-        return reverse("ShareHolder_detail", kwargs={"pk": self.pk})
+        return self.account.title
+ 
