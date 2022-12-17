68f94068e8be9d578335ccc54b12c149f9957eac
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2022-12-17 05:21:58 +0330

    ....

--- loyaltyclub/serializers.py
+++ loyaltyclub/serializers.py
@@ -4,7 +4,7 @@ from .models import Order
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model=Order
-        fields=['id','title','sum']
+        fields=['id','title','sum','persian_date_ordered']
 
 
 class CustomerSerializer(serializers.ModelSerializer):
@@ -12,5 +12,5 @@ class CustomerSerializer(serializers.ModelSerializer):
     region=AreaSerializer()
     class Meta:
         model=Customer
-        fields=['id','region','account','get_absolute_url','get_loyaltyclub_absolute_url','persian_date_ordered']
+        fields=['id','region','account','get_absolute_url','get_loyaltyclub_absolute_url']
         
